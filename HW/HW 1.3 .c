e50e03d8586a954c8268db060efea304be77f567
HemunPrawang <saber334521@gmail.com>
2024-11-24 14:13:16 +0700

    Add files via upload

---
+++ HW/HW 1.3 .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char F[100] ;
+    char L[100] ;
+    
+    printf( "First Name : " ) ;
+    scanf( "%s" , F ) ;
+    
+    printf( "Last Name : " ) ;
+    scanf( "%s" , L ) ;
+    
+    printf( "%s %ss TC, RMUTL, Chiang Mai, Thailand" , F , L ) ;
+    return 0;
+}
